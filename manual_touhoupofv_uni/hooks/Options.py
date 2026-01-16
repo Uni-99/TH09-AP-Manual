5688e6de4217f86dd2c6f4f22fa7af47bc62ee74
Uni-99 <uni30221@gmail.com>
2026-01-16 22:31:09 +0100

    APworld file updated

--- manual_touhoupofv_uni/hooks/Options.py
+++ manual_touhoupofv_uni/hooks/Options.py
@@ -25,43 +25,64 @@ from typing import Type, Any
 # To add an option, use the before_options_defined hook below and something like this:
 #   options["total_characters_to_win_with"] = TotalCharactersToWinWith
 #
+class Mode(Choice):
+    """Which mode would you like to play on?
+    Story Mode: Go through each stages to clear stage 9. Restart from stage 1 if you die more times than you have life items.
+    Match Mode: Survive long enough against computer opponents in a similar structure to story mode. Time reduction items reduce time needed to survive."""
+    display_name = "mode"
+    option_story_mode = 0
+    option_match_mode = 1
+    default = 0
+
 class EndingsRequired(Range):
-    """The number of endings required to goal"""
+    """How many ending items should be required to goal?"""
     display_name = "endings to goal"
     range_start = 1
-    range_end = 14
-    default = 4
+    range_end = 16
+    default = 1
 
-class DifficultyMid(Choice):
-    """Whether clearing stage 6, 7 and 8 requires a life item to be in logic
-    3_lives: All 3 stages requires a life each
-    2_lives: Stage 7 and 8 requires a life each
-    1_life: Stage 8 requires a life"""
-    display_name = "stage 6/7/8 lives"
+class StoryDifficultyMid(Choice):
+    """[For Story Mode] Should clearing stage 6, 7 and 8 require life items to be in logic?
+    3 Lives: 1 life item makes stage 6 in logic, 2 life items for stage 7, 3 life items for stage 8.
+    2 Lives: 1 life item makes stage 6 in logic, 2 life items for stage 7 and 8.
+    1 Lives: 1 life item makes all 3 stages in logic.
+    None: All 3 stages are in logic without the need of life items."""
+    display_name = "story mid game lives"
     option_3_lives = 3
     option_2_lives = 2
-    option_1_life = 1
+    option_1_lives = 1
     option_none = 0
     default = 3
 
-class DifficultyEnd(Range):
-    """The number of lives required for clearing stage 9 to be in logic"""
-    display_name = "stage 9 lives"
+class StoryDifficultyEnd(Range):
+    """[For Story Mode] How many life items should be required for clearing stage 9 to be in logic?"""
+    display_name = "story end game lives"
     range_start = 1
     range_end = 7
     default = 6
 
-class DifficultyAyaMedi(Toggle):
-    """Whether Aya and Medicine require any lives for their stage clears to be in logic"""
-    display_name = "aya and medicine lives"
+class MatchDifficulty(Choice):
+    """[For Match Mode] How long should lasting against opponents be required for them to be in logic?"""
+    display_name = "match time"
+    option_4min = 4
+    option_3min = 5
+    option_2min = 6
+    option_1min = 7
+    default = 6
+
+class AyaMediDifficulty(Toggle):
+    """Should Aya and Medicine require any of their progressive items for their stage clears to be in logic?"""
+    display_name = "aya and medicine progression"
     default = 1
 
 # This is called before any manual options are defined, in case you want to define your own with a clean slate or let Manual define over them
 def before_options_defined(options: dict[str, Type[Option[Any]]]) -> dict[str, Type[Option[Any]]]:
+    # options["mode"] = Mode
     # options["endings_required"] = EndingsRequired
-    # options["difficulty_mid"] = DifficultyMid
-    # options["difficulty_end"] = DifficultyEnd
-    # options["difficulty_ayamedi"] = DifficultyAyaMedi
+    # options["story_difficulty_mid"] = StoryDifficultyMid
+    # options["story_difficulty_end"] = StoryDifficultyEnd
+    # options["match_difficulty"] = MatchDifficulty
+    # options["ayamedi_difficulty"] = AyaMediDifficulty
     return options
 
 # This is called after any manual options are defined, in case you want to see what options are defined or want to modify the defined options
